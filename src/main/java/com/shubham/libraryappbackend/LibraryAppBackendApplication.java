9b12af0b55c687cc2d0152dabdff8cf241f0cf7d
Shubham <vivekgoyal164@gmail.com>
2023-11-08 19:44:48 +0530

    Book Model

---
+++ src/main/java/com/shubham/libraryappbackend/LibraryAppBackendApplication.java
@@ -0,0 +1,13 @@
+package com.shubham.libraryappbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryAppBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryAppBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shubham/libraryappbackend/LibraryAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shubham.libraryappbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryAppBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
