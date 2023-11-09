70456ba9ff497c104bd3e9a1c0c42570e6ecb145
Shubham <vivekgoyal164@gmail.com>
2023-11-09 17:06:20 +0530

    Search API

--- src/main/java/com/shubham/libraryappbackend/dao/BookRepository.java
+++ src/main/java/com/shubham/libraryappbackend/dao/BookRepository.java
@@ -1,7 +1,12 @@
 package com.shubham.libraryappbackend.dao;
 
 import com.shubham.libraryappbackend.entity.Book;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.RequestParam;
 
 public interface BookRepository extends JpaRepository<Book,Long> {
+    
+    Page<Book> findByTitleContaining(@RequestParam("title") String title, Pageable pageable);
 }
